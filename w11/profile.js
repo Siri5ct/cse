c4734953a8fefea5d8f198f56a923cc972455387
Siri5ct <avirnenisiri@gmail.com>
2025-06-01 18:06:39 +0530

    Create profile.js

---
+++ w11/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Profile = () => {
+    return (
+      <>
+        <h1>Welcome ABC</h1>
+        <p>CSE - A</p>
+      </>
+    );
+  };
+  
+  export default Profile;
+  
