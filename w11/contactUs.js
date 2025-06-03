2312dc891e527bdad94c20bc81b6937459bd467a
Siri5ct <avirnenisiri@gmail.com>
2025-06-03 17:32:41 +0530

    Delete w11 directory

--- w11/contactUs.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const ContactUs = () => {
-    return <h1>CMRIT, Kandlakoya Village, Medchal, Hyderabad</h1>;
-  };
-  
-  export default ContactUs;
-  
--- w11/home.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const Home = () => {
-    return <h1>Welcome to CMRIT Home Page</h1>;
-  };
-  
-  export default Home;
-  
